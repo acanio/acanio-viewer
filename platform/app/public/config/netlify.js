6f97ae1950e6b04c3116bc9723cd4fe50bc36b60
Aysel Afsar <aysel.afsar@gmail.com>
2024-03-18 23:00:18 -0400

    fix(ci): Fix render build

--- platform/app/public/config/netlify.js
+++ platform/app/public/config/netlify.js
@@ -4,6 +4,147 @@ window.config = {
   routerBasename: '/',
   extensions: [],
   modes: [],
+  customizationService: {
+    cornerstoneViewportActionBar: {
+      disabled: true,
+    },
+    cornerstoneOverlayTopLeft: {
+      id: 'cornerstoneOverlayTopLeft',
+      items: [
+        {
+          id: 'PatientName',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: '',
+          condition: ({ instance }) =>
+              instance && instance.PatientName && (typeof instance.PatientName === 'string' || instance.PatientName.Alphabetic),
+          contentF: ({ instance, formatters: { formatPN } }) =>
+              (typeof instance.PatientName === 'string' ? formatPN(instance.PatientName) : formatPN(instance.PatientName.Alphabetic)) +
+              ' ' +
+              (instance.PatientSex ? '(' + instance.PatientSex + ')' : ''),
+        },
+        {
+          id: 'PID',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: '',
+          title: 'Patient PID',
+          condition: ({ instance }) => instance && instance.PatientID,
+          contentF: ({ instance }) => instance.PatientID,
+        },
+        {
+          id: 'PatientBirthDate',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: 'DOB:',
+          title: "Patient's Date of birth",
+          condition: ({ instance }) => instance && instance.PatientBirthDate,
+          contentF: ({ instance }) => instance.PatientBirthDate,
+        },
+        {
+          id: 'OtherPid',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: 'Other PID:',
+          title: 'Other Patient IDs',
+          condition: ({ instance }) => instance && instance.OtherPatientIDs,
+          contentF: ({ instance, formatters: { formatPN } }) => formatPN(instance.OtherPatientIDs),
+        },
+      ],
+    },
+    cornerstoneOverlayTopRight: {
+      id: 'cornerstoneOverlayTopRight',
+
+      items: [
+        {
+          id: 'SeriesDescription',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: '',
+          title: 'Series Description',
+          condition: ({ instance }) => instance && instance.SeriesDescription,
+          contentF: ({ instance }) => instance.SeriesDescription,
+        },
+        {
+          id: 'StudyDateTime',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: '',
+          title: 'Study date',
+          condition: ({ instance }) => instance && (instance.StudyDate || instance.StudyTime),
+          contentF: ({ instance, formatters: { formatDate, formatTime } }) =>
+              [
+                instance.StudyDate ? formatDate(instance.StudyDate) : '',
+                instance.StudyTime ? formatTime(instance.StudyTime) : ''
+              ].join(' '),
+        },
+      ],
+    },
+    cornerstoneOverlayBottomLeft: {
+      id: 'cornerstoneOverlayBottomLeft',
+
+      items: [
+        {
+          id: 'SeriesNumber',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: 'Ser:',
+          title: 'Series Number',
+          condition: ({ instance }) => instance && instance.SeriesNumber,
+          contentF: ({ instance }) => instance.SeriesNumber,
+        },
+        {
+          id: 'InstanceNmber',
+          customizationType: 'ohif.overlayItem.instanceNumber',
+          color: '#fff',
+        },
+        {
+          id: 'ColumnsRows',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: '',
+          condition: ({ instance }) => instance && instance.Columns && instance.Rows,
+          contentF: ({ instance }) => `${instance.Columns} x ${instance.Rows}`,
+        },
+        {
+          id: 'SliceLocation',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: 'Loc:',
+          title: 'Slice Location',
+          condition: ({ instance }) => instance && instance.SliceLocation,
+          contentF: ({ instance, formatters: { formatNumberPrecision } }) =>
+              formatNumberPrecision(instance.SliceLocation, 2) + ' mm',
+        },
+        {
+          id: 'SliceThickness',
+          customizationType: 'ohif.overlayItem',
+          color: '#fff',
+          label: 'Thick:',
+          title: 'Slice Thickness',
+          condition: ({ instance }) => instance && instance.SliceThickness,
+          contentF: ({ instance, formatters: { formatNumberPrecision } }) =>
+              formatNumberPrecision(instance.SliceThickness, 2) + ' mm',
+        },
+      ],
+    },
+    cornerstoneOverlayBottomRight: {
+      id: 'cornerstoneOverlayBottomRight',
+
+      items: [
+        {
+          id: 'ZoomLevel',
+          customizationType: 'ohif.overlayItem.zoomLevel',
+          color: '#fff',
+        },
+        {
+          id: 'WindowLevel',
+          customizationType: 'ohif.overlayItem.windowLevel',
+          color: '#fff',
+        },
+      ],
+    },
+  },
   showStudyList: true,
   // below flag is for performance reasons, but it might not work for all servers
   showWarningMessageForCrossOrigin: true,
@@ -157,6 +298,49 @@ window.config = {
 
     // Could use services manager here to bring up a dialog/modal if needed.
     console.warn('test, navigate to https://ohif.org/');
+  },
+  whiteLabeling: {
+    /* Optional: Should return a React component to be rendered in the "Logo" section of the application's Top Navigation bar */
+    createLogoComponentFn: function (React) {
+      return React.createElement(
+          'a',
+          {
+            target: '_blank',
+            href: 'https://acan.io',
+          },
+          React.createElement(
+              'div',
+              {
+                className: 'h-8',
+                style: {
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                },
+              },
+              React.createElement(
+                  'img',
+                  {
+                    src: '/assets/acanio.svg',
+                    className: 'w-10 h-10',
+                    style: {
+                      display: 'inline-block',
+                    },
+                  }),
+              React.createElement(
+                  'span',
+                  {
+                    className: 'ml-3 text-xl',
+                    style: {
+                      color: '#fff',
+                      marginRight: '250px',
+                    },
+                  },
+                  'Acan.io'
+              )
+          )
+      );
+    },
   },
   hotkeys: [
     {
